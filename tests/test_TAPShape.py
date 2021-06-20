d90c108a0d5497664d8738ee52fa47cee7d9037d
Tom Baker <tom@tombaker.org>
2021-06-20 05:25:49 +0200

    remove "start" field from some tests (for now)

--- tests/test_TAPShape.py
+++ tests/test_TAPShape.py
@@ -3,7 +3,6 @@
 from dctap.tapclasses import TAPShape
 
 SHAPE_OBJECT = TAPShape(
-    start=True,
     shapeID=":a",
     shapeLabel="Resource",
     sc_list=[
@@ -17,7 +16,6 @@ SHAPE_OBJECT = TAPShape(
 def test_shape_fields_are_individually_addressable():
     """Fields of TAPShape instance are individually addressable."""
     shap = SHAPE_OBJECT
-    assert shap.start
     assert shap.shapeID == ":a"
     assert shap.shapeLabel == "Resource"
     assert shap.sc_list[1] == {"propertyID": "dct:subject", "valueNodeType": "IRI"}
@@ -34,7 +32,6 @@ def test_sc_list_items_are_individually_addressable():
 def test_shape_initialized_by_assignment():
     """TAPShape instances can be created by assignment."""
     shap = TAPShape()
-    shap.start = True
     shap.shapeID = ":a"
     shap.shapeLabel = "Resource"
     shap.sc_list = []
@@ -44,37 +41,9 @@ def test_shape_initialized_by_assignment():
     assert shap == SHAPE_OBJECT
 
 
-def test_shape_initialized_with_no_propertyvalues_field_should_pass_for_now():
-    """Test should pass for now but this condition should raise exception."""
-    shap = TAPShape()
-    shap.start = True
-    shap.shapeID = ":a"
-    assert shap == TAPShape(start=True, shapeID=":a")
-
-
-def test_shape_initialized_with_no_start_field_should_pass_for_now():
-    """Test should pass for now but this condition should raise exception."""
-    shap = TAPShape()
-    shap.shapeID = ":a"
-    shap.sc_list = []
-    shap.sc_list.append({"propertyID": "dct:creator", "valueNodeType": "IRI"})
-    shap.sc_list.append({"propertyID": "dct:subject", "valueNodeType": "IRI"})
-    shap.sc_list.append({"propertyID": "dct:date", "valueNodeType": "String"})
-    assert shap == TAPShape(
-        shapeID=":a",
-        sc_list=[
-            {"propertyID": "dct:creator", "valueNodeType": "IRI"},
-            {"propertyID": "dct:subject", "valueNodeType": "IRI"},
-            {"propertyID": "dct:date", "valueNodeType": "String"},
-        ],
-    )
-
-
 def test_shape_initialized_with_no_shapeid_field_should_pass_for_now():
     """Shape initialized with no shapeID will use default shapeID."""
     shap = TAPShape()
-    shap.start = True
-    shap.shapeLabel = "Default"
     shap.sc_list = []
     shap.sc_list.append({"propertyID": "dct:creator", "valueNodeType": "IRI"})
     shap.sc_list.append({"propertyID": "dct:subject", "valueNodeType": "IRI"})
@@ -83,9 +52,7 @@ def test_shape_initialized_with_no_shapeid_field_should_pass_for_now():
     config_dict['default_shape_name'] = ":default"
     shap._normalize_default_shapeID(config_dict)
     assert shap == TAPShape(
-        start=True,
         shapeID = ":default",
-        shapeLabel = "Default",
         sc_list=[
             {"propertyID": "dct:creator", "valueNodeType": "IRI"},
             {"propertyID": "dct:subject", "valueNodeType": "IRI"},

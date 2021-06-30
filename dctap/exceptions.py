3096899407d5a2fa283a275c60cdb95fdaf37059
Tom Baker <tom@tombaker.org>
2021-06-30 13:06:32 +0200

    exceptions.py - delete unused exceptions

--- dctap/exceptions.py
+++ dctap/exceptions.py
@@ -8,30 +8,3 @@ class DctapError(SystemExit):
 class ConfigError(DctapError):
     """Exceptions related to configuration."""
 
-
-class CSVRowError(DctapError):
-    """Exceptions related to a single CSVRow."""
-
-
-class UristemValueError(DctapError):
-    """Exceptions related to UriStem value."""
-
-
-class CsvError(DctapError):
-    """Exceptions related to an entire CSV-derived object."""
-
-
-class BadRegexError(SystemExit):
-    """String does not compile as regular expression."""
-
-
-class BadYamlError(SystemExit):
-    """YAML does not parse."""
-
-
-class NotUTF8Error(SystemExit):
-    """File is not UTF8-encoded."""
-
-
-class ConfigWarning(Warning):
-    """Warning regarding configuration (does not stop execution)."""

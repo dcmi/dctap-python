0936d2b8c154fdfba1c5941419db1eb50821fbb0
Tom Baker <tom@tombaker.org>
2021-07-05 07:18:53 +0200

    tests/test_config_get.py

--- tests/test_config_get.py
+++ tests/test_config_get.py
@@ -3,7 +3,7 @@
 import os
 import pytest
 from pathlib import Path
-from dctap.config import get_config, DEFAULT_CONFIGFILE_NAME
+from dctap.config import get_config
 from dctap.exceptions import ConfigError
 
 YAML_CONFIGFILE_CONTENTS = """\
@@ -14,15 +14,17 @@ prefixes:
     "dcterms:": "http://purl.org/dc/terms/"
 """
 
+DEFAULT_CONFIGFILE_NAME = "tapshex.yml"
+
 def test_get_config_from_default_config_file_if_present(tmp_path):
-    """Get config dict from config file dctap.yml if present."""
+    """Get config dict from config file DEFAULT_CONFIGFILE_NAME if present."""
     os.chdir(tmp_path)
     Path(DEFAULT_CONFIGFILE_NAME).write_text(YAML_CONFIGFILE_CONTENTS)
-    assert get_config()["prefixes"] == {
+    assert get_config(configfile=DEFAULT_CONFIGFILE_NAME, defaults_yaml=YAML_CONFIGFILE_CONTENTS)["prefixes"] == {
         ":": "http://example.org/",
         "dcterms:": "http://purl.org/dc/terms/",
     }
-    assert get_config() == {
+    assert get_config(configfile=DEFAULT_CONFIGFILE_NAME, defaults_yaml=YAML_CONFIGFILE_CONTENTS) == {
         "default_shape_name": ":default",
         "prefixes": { ":": "http://example.org/",
                       "dcterms:": "http://purl.org/dc/terms/",
@@ -39,7 +41,7 @@ def test_get_default_config_settings_if_configfile_not_found(tmp_path):
 def test_exit_if_configfile_has_bad_yaml(tmp_path):
     """Raise exception if config file has bad YAML."""
     os.chdir(tmp_path)
-    BAD_CONFIG_YAML = "DELIBE\nRATELY BAD: -: ^^YAML CONTENT^^\n"
-    Path(DEFAULT_CONFIGFILE_NAME).write_text(BAD_CONFIG_YAML)
+    bad_config_yaml = "DELIBE\nRATELY BAD: -: ^^YAML CONTENT^^\n"
+    Path(DEFAULT_CONFIGFILE_NAME).write_text(bad_config_yaml)
     with pytest.raises(ConfigError):
-        get_config()
+        get_config(configfile=DEFAULT_CONFIGFILE_NAME, defaults_yaml=YAML_CONFIGFILE_CONTENTS)

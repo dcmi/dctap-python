1bfb29acd0b1f8d0f44dbba823877455c9f0dce4
Tom Baker <tom@tombaker.org>
2021-07-06 08:39:57 +0200

    config.py - get_config write_config now take settings dict as argument

--- dctap/config.py
+++ dctap/config.py
@@ -1,10 +1,13 @@
 """Default settings."""
 
 import sys
+from dataclasses import asdict
 from pathlib import Path
+
 # pylint: disable=consider-using-from-import
 import ruamel.yaml as yaml
 from .exceptions import ConfigError
+from .tapclasses import TAPShape, TAPStatementConstraint
 
 
 DEFAULT_CONFIG_YAML = """# dctap configuration file (in YAML format)
@@ -32,45 +35,106 @@ prefixes:
 
 # Aliases (case-insensitive) mapped to "official" element names (case-sensitive)
 element_aliases:
-    "Prop ID": "propertyID"
-    "Mand": "mandatory"
-    "Rep": "repeatable"
-    "Node Type": "valueNodeType"
-    "Datatype": "valueDataType"
-    "VC": "valueConstraint"
-    "VCType": "valueConstraintType"
-    "VShape": "valueConstraintType"
+    "propid": "propertyID"
+    "mand": "mandatory"
+    "rep": "repeatable"
+    "nodetype": "valueNodeType"
+    "datatype": "valueDataType"
+    "vc": "valueConstraint"
+    "vctype": "valueConstraintType"
+    "vshape": "valueShape"
 """
 
 DEFAULT_CONFIGFILE_NAME = "dctap.yml"
 
 
-def get_config(configfile=DEFAULT_CONFIGFILE_NAME, defaults_yaml=DEFAULT_CONFIG_YAML):
-    """Get config dict from file if found, else get built-in defaults."""
-    # pylint: disable=raise-missing-from
-    bad_form = f"{repr(configfile)} is badly formed: fix, re-generate, or delete."
-    not_found = f"{repr(configfile)} not found or not readable."
-    try:
-        return yaml.safe_load(Path(configfile).read_text())
-    except (FileNotFoundError, PermissionError):
-        if configfile != DEFAULT_CONFIGFILE_NAME:
-            raise ConfigError(not_found)
-    except (yaml.YAMLError, yaml.scanner.ScannerError):
-        raise ConfigError(bad_form)
-    return yaml.safe_load(defaults_yaml)
-
-
-def write_configfile(configfile=DEFAULT_CONFIGFILE_NAME, defaults_yaml=DEFAULT_CONFIG_YAML):
+def write_configfile(
+    config_file=DEFAULT_CONFIGFILE_NAME,
+    config_yaml=DEFAULT_CONFIG_YAML,
+    shape_class=TAPShape,
+    statement_constraint_class=TAPStatementConstraint,
+):
     """Write initial config file, by default to CWD, or exit if already exists."""
-    if Path(configfile).exists():
-        raise ConfigError(f"{repr(configfile)} already exists - will not overwrite.")
+    if Path(config_file).exists():
+        raise ConfigError(f"{repr(config_file)} already exists - will not overwrite.")
     try:
-        with open(configfile, "w", encoding="utf-8") as outfile:
-            outfile.write(defaults_yaml)
+        with open(config_file, "w", encoding="utf-8") as outfile:
+            outfile.write(config_yaml)
             print(
-                f"Built-in settings written to {str(configfile)} - edit as needed.",
+                f"Built-in settings written to {str(config_file)} - edit as needed.",
                 file=sys.stderr,
             )
     except FileNotFoundError:
         # pylint: disable=raise-missing-from
-        raise ConfigError(f"{repr(configfile)} is not writeable - try different name.")
+        raise ConfigError(f"{repr(config_file)} is not writeable - try different name.")
+
+
+def get_config(
+    config_file=None,
+    nondefault_config_yaml=None,
+    shape_class=TAPShape,
+    statement_constraint_class=TAPStatementConstraint,
+):
+    """Get config dict from file if found, else get built-in defaults."""
+    # pylint: disable=raise-missing-from
+    elements_dict = dict()
+    elements_dict["shape_elements"] = _shape_elements(shape_class)
+    elements_dict["statement_constraint_elements"] = _statement_constraint_elements(
+        statement_constraint_class
+    )
+    elements_dict["csv_elements"] = (
+        elements_dict["shape_elements"] + elements_dict["statement_constraint_elements"]
+    )
+    bad_form = f"{repr(config_file)} is badly formed: fix, re-generate, or delete."
+    not_found = f"{repr(config_file)} not found or not readable."
+    if config_file: # if a specific config file was named
+        try:
+            config_yaml = Path(config_file).read_text()
+        except (FileNotFoundError, PermissionError):
+            raise ConfigError(not_found)
+    else:
+        try:
+            config_yaml = Path(DEFAULT_CONFIGFILE_NAME).read_text()
+        except (FileNotFoundError, PermissionError):
+            if nondefault_config_yaml:
+                config_yaml = nondefault_config_yaml
+            else:
+                config_yaml = DEFAULT_CONFIG_YAML
+    try:
+        config_dict = yaml.safe_load(config_yaml)
+    except (yaml.YAMLError, yaml.scanner.ScannerError):
+        raise ConfigError(bad_form)
+    config_dict.update(elements_dict)
+    if not config_dict.get("element_aliases"):   # is this necessary?
+        config_dict["element_aliases"] = dict()  # is this necessary?
+    config_dict["element_aliases"].update(
+        _compute_alias2element_mappings(config_dict["csv_elements"])
+    )
+    return config_dict
+
+
+def _compute_alias2element_mappings(csv_elements_list=None):
+    """Compute shortkey/lowerkey-to-element mappings from list of CSV elements."""
+    alias2element_mappings = dict()
+    for csv_elem in csv_elements_list:
+        # shortkey: initial letter (lowercase) + each uppercase letter, lowercased
+        shortkey = "".join([csv_elem[0]] + [l.lower() for l in csv_elem if l.isupper()])
+        lowerkey = csv_elem.lower()
+        alias2element_mappings[shortkey] = csv_elem  # { shortkey: camelcasedValue }
+        alias2element_mappings[lowerkey] = csv_elem  # { lowerkey: camelcasedValue }
+    return alias2element_mappings
+
+
+def _shape_elements(shape_class=TAPShape):
+    """List DCTAP elements supported by given shape class."""
+    sh_elements = list(asdict(shape_class()))
+    sh_elements.remove("sc_list")
+    sh_elements.remove("sh_warnings")
+    return sh_elements
+
+
+def _statement_constraint_elements(statement_constraint_class=TAPStatementConstraint):
+    """List DCTAP elements supported by given statement constraint class."""
+    sc_elements = list(asdict(statement_constraint_class()))
+    sc_elements.remove("sc_warnings")
+    return sc_elements

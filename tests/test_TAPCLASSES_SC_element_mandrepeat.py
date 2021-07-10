3d0030e3a2b5b010e2c2810348713975f98c1294
Tom Baker <tom@tombaker.org>
2021-07-10 16:30:40 +0200

    remove another fiddly test of printed output

--- tests/test_TAPCLASSES_SC_element_mandrepeat.py
+++ tests/test_TAPCLASSES_SC_element_mandrepeat.py
@@ -27,51 +27,13 @@ def test_mandatory_and_repeatable_one_zero_normalized_to_true_false():
     assert sc.mandatory is "True"
     assert sc.repeatable is "False"
 
-def test_mandatory_and_repeatable_default_to_none():
-    """The Boolean elements default to None if no value is declared."""
+def test_mandatory_and_repeatable_default_to_empty_strings():
+    """The Boolean elements remain empty strings if no value is declared."""
     sc = TAPStatementConstraint()
     sc.propertyID = "wdt:P31"
     sc._normalize_booleans_mandatory_repeatable()
-    assert sc.mandatory is None
-    assert sc.repeatable is None
-
-@pytest.mark.skip
-def test_booleans_shown_as_True_False_in_text_display():
-    """Booleans display in text output as "True" and "False"."""
-    SHAPES_DICT = {'shapes': [{'sh_warnings': {},
-             'shapeID': 'default',
-             'shapeLabel': '',
-             'statement_constraints': [{'mandatory': "True",
-                                        'note': '',
-                                        'propertyID': ':creator',
-                                        'propertyLabel': '',
-                                        'repeatable': "False",
-                                        'sc_warnings': {},
-                                        'valueConstraint': '',
-                                        'valueConstraintType': '',
-                                        'valueDataType': '',
-                                        'valueNodeType': '',
-                                        'valueShape': ''}]}]}
-    expected_output_list = [
-        "DCTAP instance",
-        "    Shape",
-        "        shapeID:                 default",
-        "        Statement Constraint",
-        "            propertyID:          :creator",
-        "            mandatory:           True",
-        "            repeatable:          False",
-    ]
-    SETTINGS_DICT = get_config()
-    pprint_tapshapes_output = pprint_tapshapes(SHAPES_DICT, SETTINGS_DICT)
-    assert(SETTINGS_DICT["extra_shape_elements"])
-    assert "min" in SETTINGS_DICT["extra_statement_constraint_elements"]
-    assert "max" in SETTINGS_DICT["extra_statement_constraint_elements"]
-    assert len(expected_output_list) == 7
-    assert type(expected_output_list) == list
-    assert expected_output_list[0] == "DCTAP instance"
-    assert type(pprint_tapshapes_output) == list
-    assert len(pprint_tapshapes_output) == 7
-    # assert pprint_tapshapes_output == expected_output_list
+    assert sc.mandatory == ""
+    assert sc.repeatable == ""
 
 def test_mandatory_and_repeatable_raise_warn_unsupported_boolean_values():
     """@@@"""

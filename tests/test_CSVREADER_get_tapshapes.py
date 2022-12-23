8ba7834fa5fc8ef67955d29fb00e47960c1feae1
Tom Baker <tom@tombaker.org>
2022-12-23 15:15:12 +0100

    test_CSVREADER_get_tapshapes.py - _get_tapshapes now called with state_class, shape_class

--- tests/test_CSVREADER_get_tapshapes.py
+++ tests/test_CSVREADER_get_tapshapes.py
@@ -3,9 +3,9 @@
 import pytest
 from dctap.config import get_config
 from dctap.csvreader import _get_tapshapes
+from dctap.tapclasses import TAPShape, TAPStatementTemplate
 
 SETTINGS_DICT = get_config()
-
 
 def test_get_tapshapes_two_shapes_with_rows_that_are_ignored():
     """Lines without shapeID and/or propertyID are ignored."""
@@ -17,12 +17,18 @@ def test_get_tapshapes_two_shapes_with_rows_that_are_ignored():
         {"shapeID": ":author"},
         {"propertyID": "foaf:name"},
     ]
-    (shapes, warns) = _get_tapshapes(rows, SETTINGS_DICT)
+    (shapes, warns) = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     assert len(shapes["shapes"]) == 2
     assert shapes["shapes"][0]["shapeID"] == ":bookshape"
     assert len(shapes["shapes"][0]["statement_templates"]) == 1
     assert shapes["shapes"][1]["shapeID"] == ":author"
     assert len(shapes["shapes"][1]["statement_templates"]) == 1
+
 
 def test_get_tapshapes_two_shapes_declare_on_separate_rows():
     """Shape declared in own rows followed by rows with statement templates."""
@@ -32,12 +38,18 @@ def test_get_tapshapes_two_shapes_declare_on_separate_rows():
         {"shapeID": ":author"},
         {"propertyID": "foaf:name"},
     ]
-    (shapes, warns) = _get_tapshapes(rows, SETTINGS_DICT)
+    (shapes, warns) = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     assert len(shapes["shapes"]) == 2
     assert shapes["shapes"][0]["shapeID"] == ":bookshape"
     assert len(shapes["shapes"][0]["statement_templates"]) == 1
     assert shapes["shapes"][1]["shapeID"] == ":author"
     assert len(shapes["shapes"][1]["statement_templates"]) == 1
+
 
 def test_get_tapshapes_shape_elements_declared_on_separate_row():
     """One shape, declared on its own row."""
@@ -45,12 +57,18 @@ def test_get_tapshapes_shape_elements_declared_on_separate_row():
         {"shapeID": ":bookshape", "shapeLabel": "Book"},
         {"propertyID": "dc:creator"},
     ]
-    tapshapes_output = _get_tapshapes(rows, SETTINGS_DICT)
+    tapshapes_output = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     expected_shapes = tapshapes_output[0]
     assert len(expected_shapes) == 1
     assert expected_shapes["shapes"][0]["shapeID"] == ":bookshape"
     assert expected_shapes["shapes"][0]["shapeLabel"] == "Book"
     assert len(expected_shapes["shapes"][0]["statement_templates"]) == 1
+
 
 def test_get_tapshapes_one_default_shape_with_shapeID_asserted():
     """One default shape with shapeID asserted."""
@@ -59,11 +77,17 @@ def test_get_tapshapes_one_default_shape_with_shapeID_asserted():
         {"shapeID": "", "propertyID": "dc:date"},
         {"shapeID": "", "propertyID": "dc:subject"},
     ]
-    tapshapes_output = _get_tapshapes(rows, SETTINGS_DICT)
+    tapshapes_output = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     expected_shapes = tapshapes_output[0]
     assert len(expected_shapes) == 1
     assert expected_shapes["shapes"][0]["shapeID"] == "default"
     assert len(expected_shapes["shapes"][0]["statement_templates"]) == 3
+
 
 def test_get_tapshapes_one_default_shape_with_shapeID_not_asserted():
     """One default shape with shapeID not asserted."""
@@ -72,11 +96,17 @@ def test_get_tapshapes_one_default_shape_with_shapeID_not_asserted():
         {"propertyID": "dc:date"},
         {"propertyID": "dc:subject"},
     ]
-    tapshapes_output = _get_tapshapes(rows, SETTINGS_DICT)
+    tapshapes_output = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     expected_shapes = tapshapes_output[0]
     assert len(expected_shapes) == 1
     assert expected_shapes["shapes"][0]["shapeID"] == "default"
     assert len(expected_shapes["shapes"][0]["statement_templates"]) == 3
+
 
 def test_get_tapshapes_two_shapes_first_is_default():
     """Two shapes, first of which is default."""
@@ -85,11 +115,17 @@ def test_get_tapshapes_two_shapes_first_is_default():
         {"shapeID": "", "propertyID": "dc:type"},
         {"shapeID": ":author", "propertyID": "foaf:name"},
     ]
-    tapshapes_output = _get_tapshapes(rows, SETTINGS_DICT)
+    tapshapes_output = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     expected_shapes = tapshapes_output[0]
     assert expected_shapes["shapes"][0]["shapeID"] == "default"
     assert expected_shapes["shapes"][1]["shapeID"] == ":author"
     assert len(expected_shapes["shapes"][0]["statement_templates"]) == 2
+
 
 def test_get_tapshapes_two_shapes_where_rows_are_out_of_order():
     """Two shapes in three rows, in mixed order (ABA)."""
@@ -98,12 +134,18 @@ def test_get_tapshapes_two_shapes_where_rows_are_out_of_order():
         {"shapeID": ":author", "propertyID": "foaf:name"},
         {"shapeID": ":book", "propertyID": "dc:type"},
     ]
-    tapshapes_output = _get_tapshapes(rows, SETTINGS_DICT)
+    tapshapes_output = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     expected_shapes = tapshapes_output[0]
     assert expected_shapes["shapes"][0]["shapeID"] == ":book"
     assert expected_shapes["shapes"][1]["shapeID"] == ":author"
     assert len(expected_shapes["shapes"][0]["statement_templates"]) == 2
     assert len(expected_shapes["shapes"][1]["statement_templates"]) == 1
+
 
 def test_get_tapshapes_two_shapes_spelled_out_entirely():
     """Two shapes, first of which is default because shapeID is empty."""
@@ -112,55 +154,79 @@ def test_get_tapshapes_two_shapes_spelled_out_entirely():
         {"shapeID": "", "propertyID": "dc:type"},
         {"shapeID": ":author", "propertyID": "foaf:name"},
     ]
-    expected_shapes = {'shapes': [{'shape_warns': {},
-                 'shapeID': 'default',
-                 'shapeLabel': '',
-                 'statement_templates': [{'mandatory': None,
-                                            'note': '',
-                                            'propertyID': 'dc:creator',
-                                            'propertyLabel': '',
-                                            'repeatable': None,
-                                            'state_warns': {},
-                                            'valueConstraint': '',
-                                            'valueConstraintType': '',
-                                            'valueDataType': '',
-                                            'valueNodeType': '',
-                                            'valueShape': ''},
-                                           {'mandatory': None,
-                                            'note': '',
-                                            'propertyID': 'dc:type',
-                                            'propertyLabel': '',
-                                            'repeatable': None,
-                                            'state_warns': {},
-                                            'valueConstraint': '',
-                                            'valueConstraintType': '',
-                                            'valueDataType': '',
-                                            'valueNodeType': '',
-                                            'valueShape': ''}]},
-                {'shape_warns': {},
-                 'shapeID': ':author',
-                 'shapeLabel': '',
-                 'statement_templates': [{'mandatory': None,
-                                            'note': '',
-                                            'propertyID': 'foaf:name',
-                                            'propertyLabel': '',
-                                            'repeatable': None,
-                                            'state_warns': {},
-                                            'valueConstraint': '',
-                                            'valueConstraintType': '',
-                                            'valueDataType': '',
-                                            'valueNodeType': '',
-                                            'valueShape': ''}]}]}
-    tapshapes_output = _get_tapshapes(rows, SETTINGS_DICT)
+    expected_shapes = {
+        "shapes": [
+            {
+                "shape_warns": {},
+                "shapeID": "default",
+                "shapeLabel": "",
+                "statement_templates": [
+                    {
+                        "mandatory": None,
+                        "note": "",
+                        "propertyID": "dc:creator",
+                        "propertyLabel": "",
+                        "repeatable": None,
+                        "state_warns": {},
+                        "valueConstraint": "",
+                        "valueConstraintType": "",
+                        "valueDataType": "",
+                        "valueNodeType": "",
+                        "valueShape": "",
+                    },
+                    {
+                        "mandatory": None,
+                        "note": "",
+                        "propertyID": "dc:type",
+                        "propertyLabel": "",
+                        "repeatable": None,
+                        "state_warns": {},
+                        "valueConstraint": "",
+                        "valueConstraintType": "",
+                        "valueDataType": "",
+                        "valueNodeType": "",
+                        "valueShape": "",
+                    },
+                ],
+            },
+            {
+                "shape_warns": {},
+                "shapeID": ":author",
+                "shapeLabel": "",
+                "statement_templates": [
+                    {
+                        "mandatory": None,
+                        "note": "",
+                        "propertyID": "foaf:name",
+                        "propertyLabel": "",
+                        "repeatable": None,
+                        "state_warns": {},
+                        "valueConstraint": "",
+                        "valueConstraintType": "",
+                        "valueDataType": "",
+                        "valueNodeType": "",
+                        "valueShape": "",
+                    }
+                ],
+            },
+        ]
+    }
+    tapshapes_output = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     expected_shapes = tapshapes_output[0]
     assert expected_shapes["shapes"][0]["shapeID"] == "default"
     assert expected_shapes["shapes"][1]["shapeID"] == ":author"
     assert len(expected_shapes["shapes"][0]["statement_templates"]) == 2
-    assert type(_get_tapshapes(rows, SETTINGS_DICT)[0]) == dict
-    assert type(_get_tapshapes(rows, SETTINGS_DICT)[0]["shapes"]) == list
-    assert type(_get_tapshapes(rows, SETTINGS_DICT)[0]["shapes"][0]) == dict
-    assert type(_get_tapshapes(rows, SETTINGS_DICT)[0]["shapes"][0]["statement_templates"]) == list
-    assert type(_get_tapshapes(rows, SETTINGS_DICT)[0]["shapes"][0]["statement_templates"][0]) == dict
+    assert type(tapshapes_output[0]) == dict
+    assert type(tapshapes_output[0]["shapes"]) == list
+    assert type(tapshapes_output[0]["shapes"][0]) == dict
+    assert type(tapshapes_output[0]["shapes"][0]["statement_templates"]) == list
+    assert type(tapshapes_output[0]["shapes"][0]["statement_templates"][0]) == dict
+
 
 def test_get_tapshapes_two_shapes_shapeID_most_recently_used():
     """Two shapes, where shapeID left blank assigned "most recently used"."""
@@ -169,24 +235,32 @@ def test_get_tapshapes_two_shapes_shapeID_most_recently_used():
         {"shapeID": "", "propertyID": "dc:type"},
         {"shapeID": ":author", "propertyID": "foaf:name"},
     ]
-    expected_shapes = {'shapes': [
-                        {'shapeID': ':book',
-                         'statement_templates': [
-                             {'propertyID': 'dc:creator'},
-                             {'propertyID': 'dc:type'}
-                         ] 
-                        }, {'shapeID': ':author',
-                          'statement_templates': [
-                             {'propertyID': 'foaf:name'}
-                          ]
-                        }
-                      ]
-                    }
-    tapshapes_output = _get_tapshapes(rows, SETTINGS_DICT)
+    expected_shapes = {
+        "shapes": [
+            {
+                "shapeID": ":book",
+                "statement_templates": [
+                    {"propertyID": "dc:creator"},
+                    {"propertyID": "dc:type"},
+                ],
+            },
+            {
+                "shapeID": ":author",
+                "statement_templates": [{"propertyID": "foaf:name"}],
+            },
+        ]
+    }
+    tapshapes_output = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     expected_shapes = tapshapes_output[0]
     assert expected_shapes["shapes"][0]["shapeID"] == ":book"
     assert expected_shapes["shapes"][1]["shapeID"] == ":author"
     assert len(expected_shapes["shapes"][0]["statement_templates"]) == 2
+
 
 def test_get_tapshapes_two_shapes_shapeID_not_always_asserted():
     """Two shapes, where one line does not assert shapeID at all"."""
@@ -195,20 +269,27 @@ def test_get_tapshapes_two_shapes_shapeID_not_always_asserted():
         {"propertyID": "dc:type"},
         {"shapeID": ":author", "propertyID": "foaf:name"},
     ]
-    expected_shapes = {'shapes': [
-                        {'shapeID': ':book',
-                         'statement_templates': [
-                             {'propertyID': 'dc:creator'},
-                             {'propertyID': 'dc:type'}
-                         ] 
-                        }, {'shapeID': ':author',
-                          'statement_templates': [
-                             {'propertyID': 'foaf:name'}
-                          ]
-                        }
-                      ]
-                    }
-    tapshapes_output = _get_tapshapes(rows, SETTINGS_DICT)
+    expected_shapes = {
+        "shapes": [
+            {
+                "shapeID": ":book",
+                "statement_templates": [
+                    {"propertyID": "dc:creator"},
+                    {"propertyID": "dc:type"},
+                ],
+            },
+            {
+                "shapeID": ":author",
+                "statement_templates": [{"propertyID": "foaf:name"}],
+            },
+        ]
+    }
+    tapshapes_output = _get_tapshapes(
+        rows=rows,
+        config_dict=SETTINGS_DICT,
+        shape_class=TAPShape,
+        state_class=TAPStatementTemplate,
+    )
     expected_shapes = tapshapes_output[0]
     assert expected_shapes["shapes"][0]["shapeID"] == ":book"
     assert expected_shapes["shapes"][1]["shapeID"] == ":author"

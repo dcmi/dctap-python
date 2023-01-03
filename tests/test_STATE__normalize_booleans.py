5570369883e9c816832e625ad7946c76308fe38e
Tom Baker <tom@tombaker.org>
2023-01-03 12:23:40 +0100

    test_STATE__normalize_booleans.py

--- tests/test_STATE__normalize_booleans.py
+++ tests/test_STATE__normalize_booleans.py
@@ -1,10 +1,11 @@
 """Test for elements mandatory and repeatable."""
 
-import pytest
 from textwrap import dedent
+import pytest
 from dctap.config import get_config
 from dctap.tapclasses import TAPStatementTemplate
 from dctap.inspect import pprint_tapshapes
+
 
 def test_mandatory_repeatable_true_given_supported_boolean_values():
     """Literal 'True' (case-insensitive) is a supported Boolean value."""
@@ -12,8 +13,9 @@ def test_mandatory_repeatable_true_given_supported_boolean_values():
     sc.mandatory = "true"
     sc.repeatable = "FALSE"
     sc._normalize_booleans()
-    assert sc.mandatory is "true"
-    assert sc.repeatable is "false"
+    assert sc.mandatory == "true"
+    assert sc.repeatable == "false"
+
 
 def test_mandatory_and_repeatable_one_zero_normalized_to_true_false():
     """The integers 0 and 1 are supported Boolean values."""
@@ -21,8 +23,9 @@ def test_mandatory_and_repeatable_one_zero_normalized_to_true_false():
     sc.mandatory = "1"
     sc.repeatable = "0"
     sc._normalize_booleans()
-    assert sc.mandatory is "true"
-    assert sc.repeatable is "false"
+    assert sc.mandatory == "true"
+    assert sc.repeatable == "false"
+
 
 def test_mandatory_and_repeatable_default_to_empty_strings():
     """The Boolean elements remain empty strings if no value is declared."""
@@ -31,6 +34,7 @@ def test_mandatory_and_repeatable_default_to_empty_strings():
     assert sc.mandatory == ""
     assert sc.repeatable == ""
 
+
 def test_mandatory_and_repeatable_raise_warn_unsupported_boolean_values():
     """Warn about unsupported Boolean values."""
     sc = TAPStatementTemplate()
@@ -38,5 +42,5 @@ def test_mandatory_and_repeatable_raise_warn_unsupported_boolean_values():
     sc.repeatable = "WAHR"
     sc._normalize_booleans()
     assert len(sc.state_warns) == 2
-    assert sc.mandatory is "WAHR"
-    assert sc.repeatable is "WAHR"
+    assert sc.mandatory == "WAHR"
+    assert sc.repeatable == "WAHR"

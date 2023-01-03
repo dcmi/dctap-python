a88e0c4292720cc2693b8d36d16ab7a4ae2c4b99
Tom Baker <tom@tombaker.org>
2023-01-03 10:33:57 +0100

    test_issue18.py

---
+++ tests/test_issue18.py
@@ -0,0 +1,34 @@
+"""See https://github.com/dcmi/dctap-python/issues/18#issue-1274937034 ."""
+
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import pytest
+from dctap.config import get_config
+from dctap.csvreader import csvreader, _get_rows
+from dctap.defaults import CONFIGYAML, CONFIGFILE
+
+PHIL_CONFIGYAML = """### dctap configuration file (in YAML format)
+extra_statement_template_elements:
+ - severity
+
+element_aliases:
+     "Mand": "mandatory"
+     "Rep": "repeatable"
+"""
+
+def test_get_config_from_phils_yamldoc(tmp_path):
+    """Get config dict when passed Phil's YAML."""
+    os.chdir(tmp_path)
+    Path(CONFIGFILE).write_text(PHIL_CONFIGYAML)
+    config_dict = get_config()
+    assert config_dict["element_aliases"] == {'Mand': 'mandatory', 'Rep': 'repeatable'}
+    assert config_dict["extra_statement_template_elements"] == [ 'severity' ]
+
+def test_get_element_aliases_from_default_yamldoc(tmp_path):
+    """Get config dict (with element aliases) directly from built-in defaults."""
+    os.chdir(tmp_path)
+    config_dict = get_config(configyaml=CONFIGYAML)
+    assert config_dict["element_aliases"]["propertyid"] == "propertyID"
+    assert config_dict["element_aliases"]["mandatory"] == "mandatory"
+    assert config_dict["element_aliases"]["repeatable"] == "repeatable"

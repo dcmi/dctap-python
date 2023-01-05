bae8d1c4eb75052f657d16d46b49457e25b0c7ed
Tom Baker <tom@tombaker.org>
2023-01-05 11:08:29 +0100

    test_CSVREADER__make_shape.py

--- tests/test_CSVREADER__make_shape.py
+++ tests/test_CSVREADER__make_shape.py
@@ -6,6 +6,7 @@ from dctap.config import get_config
 from dctap.csvreader import _make_shape
 from dctap.tapclasses import TAPShape
 
+
 def test_make_shapes_returns_tapshape_object_even_in_absence_of_propertyID(tmp_path):
     """Populates TAPShape object even in the absence of a propertyID."""
     os.chdir(tmp_path)  # precaution to avoid interference among pytests
@@ -21,7 +22,7 @@ def test_make_shapes_returns_tapshape_object_even_in_absence_of_propertyID(tmp_p
         shapeID=":a", shapeLabel="Book", state_list=[], shape_warns={}, shape_extras={}
     )
 
-@pytest.mark.skip(reason="Will remove decorator")
+
 def test_make_shape_recognizes_only_shape_elements_so_configured(tmp_path):
     """Populates TAPShape object but ignores any statement template elements in row."""
     os.chdir(tmp_path)  # precaution to avoid interference among pytests
@@ -33,7 +34,9 @@ def test_make_shape_recognizes_only_shape_elements_so_configured(tmp_path):
         "closed": False,
         "start": True,
     }
-    assert _make_shape(row_dict=one_row, config_dict=config_dict) == TAPShape(
+    assert _make_shape(
+        row_dict=one_row, config_dict=config_dict, shape_class=TAPShape
+    ) == TAPShape(
         shapeID=":a",
         shapeLabel="Book",
         state_list=[],
@@ -42,7 +45,6 @@ def test_make_shape_recognizes_only_shape_elements_so_configured(tmp_path):
     )
 
 
-@pytest.mark.skip(reason="Will remove decorator")
 def test_make_shape_reads_all_extra_shape_elements_so_configured(tmp_path):
     """Reads all elements configured as extra shape elements."""
     os.chdir(tmp_path)  # precaution to avoid interference among pytests
@@ -56,7 +58,7 @@ def test_make_shape_reads_all_extra_shape_elements_so_configured(tmp_path):
         "start": True,
     }
     assert _make_shape(
-        row_dict=one_row, config_dict=config_dict, hardwired_shapeclass=TAPShape
+        row_dict=one_row, config_dict=config_dict, shape_class=TAPShape
     ) == TAPShape(
         shapeID=":a",
         shapeLabel="Book",
@@ -66,7 +68,6 @@ def test_make_shape_reads_all_extra_shape_elements_so_configured(tmp_path):
     )
 
 
-@pytest.mark.skip(reason="Will remove decorator")
 def test_make_shape_sets_shape_elements_only(tmp_path):
     """Populates TAPShape object but ignores any statement template elements in row."""
     os.chdir(tmp_path)  # precaution to avoid interference among pytests
@@ -80,9 +81,7 @@ def test_make_shape_sets_shape_elements_only(tmp_path):
         "propertyID": "ex:name",
         "valueNodeType": "literal",
     }
-    shape = _make_shape(
-        row_dict=one_row, config_dict=config_dict, hardwired_shapeclass=TAPShape
-    )
+    shape = _make_shape(row_dict=one_row, config_dict=config_dict, shape_class=TAPShape)
     assert shape.shapeID == ":a"
     assert shape.shapeLabel == "Book"
     # pylint: disable=use-implicit-booleaness-not-comparison
@@ -91,7 +90,6 @@ def test_make_shape_sets_shape_elements_only(tmp_path):
     assert shape.state_list == []  # _make_shape() sets shape fields only, not ST fields
 
 
-@pytest.mark.skip(reason="Will remove decorator")
 def test_make_shape_extra_shape_elements_that_are_empty_are_passed_through(tmp_path):
     """Empty shape elements are passed through, but not unasserted elements."""
     os.chdir(tmp_path)  # precaution to avoid interference among pytests
@@ -104,7 +102,7 @@ def test_make_shape_extra_shape_elements_that_are_empty_are_passed_through(tmp_p
         "closed": "",
     }
     assert _make_shape(
-        row_dict=one_row, config_dict=config_dict, hardwired_shapeclass=TAPShape
+        row_dict=one_row, config_dict=config_dict, shape_class=TAPShape
     ) == TAPShape(
         shapeID=":a",
         shapeLabel="",

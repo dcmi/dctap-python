8e892f5eb1cc506a7782f5bf23d3492b2eabf9f3
Tom Baker <tom@tombaker.org>
2023-01-05 07:19:24 +0100

    test_CONFIG_get_shems_stems.py

--- tests/test_CONFIG_get_shems_stems.py
+++ tests/test_CONFIG_get_shems_stems.py
@@ -3,16 +3,15 @@
 import pytest
 from dataclasses import asdict
 from dctap.config import _get_shems, _get_stems
+from dctap.tapclasses import TAPShape, TAPStatementTemplate
 
 
-@pytest.mark.skip(reason="Will remove decorator")
 def test_get_TAPShape_elements_when_no_config_dict_specified():
     """List TAPShape elements (minus shape_warns and state_list)."""
     expected = ["shapeID", "shapeLabel"]
-    assert _get_shems() == expected
+    assert _get_shems(TAPShape) == expected
 
 
-@pytest.mark.skip(reason="Will remove decorator")
 def test_get_TAPStatementTemplate_elements_when_no_config_dict_specified():
     """List TAPStatementTemplate elements (minus state_warns)."""
     expected = [
@@ -27,4 +26,4 @@ def test_get_TAPStatementTemplate_elements_when_no_config_dict_specified():
         "valueShape",
         "note",
     ]
-    assert sorted(_get_stems()) == sorted(expected)
+    assert sorted(_get_stems(TAPStatementTemplate)) == sorted(expected)

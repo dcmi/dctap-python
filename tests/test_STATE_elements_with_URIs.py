087119e709d2a3782ea31284c6b53cc4ff34ede6
Tom Baker <tom@tombaker.org>
2023-01-08 10:27:21 +0100

    test_STATE_elements_with_URIs.py

--- tests/test_STATE_elements_with_URIs.py
+++ tests/test_STATE_elements_with_URIs.py
@@ -12,17 +12,14 @@ Does not test:
 import pytest
 from dctap.tapclasses import TAPStatementTemplate, TAPShape
 
-
 def test_warn_if_propertyID_not_URI():
     """Values of propertyID should be IRI-like."""
     sc = TAPStatementTemplate()
     sc.propertyID = "P31"
     sc._warn_if_value_not_urilike()
-    print(sc.state_warns)
-    print(dict(sc.state_warns))
-    print(len(dict(sc.state_warns)))
     assert len(dict(sc.state_warns)) == 1
-
+    warning = "Value 'P31' does not look like a URI."
+    assert sc.state_warns["propertyID"] == warning
 
 def test_warn_if_valueDataType_not_URI():
     """Values of valueDataType should be IRI-like."""
@@ -30,18 +27,13 @@ def test_warn_if_valueDataType_not_URI():
     sc.propertyID = "wdt:P31"
     sc.valueDataType = "date"
     sc._warn_if_value_not_urilike()
-    print(sc.state_warns)
-    print(dict(sc.state_warns))
-    print(len(dict(sc.state_warns)))
     assert len(dict(sc.state_warns)) == 1
-
+    assert sc.state_warns["valueDataType"] == "Value 'date' does not look like a URI."
 
 def test_warn_if_shapeID_not_URI():
     """Values of shapeID should commonly be IRI-like."""
     sh = TAPShape()
     sh.shapeID = "FoodShape"
     sh._warn_if_value_not_urilike()
-    print(sh.shape_warns)
-    print(dict(sh.shape_warns))
-    print(len(dict(sh.shape_warns)))
     assert len(dict(sh.shape_warns)) == 1
+    assert sh.shape_warns["shapeID"] == "Value 'FoodShape' does not look like a URI."

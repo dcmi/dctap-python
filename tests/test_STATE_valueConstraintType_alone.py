e7012852c3e4dd74cfd96950daa95dbd1c32ebe7
Tom Baker <tom@tombaker.org>
2023-01-08 09:37:22 +0100

    test_STATE_valueConstraintType_alone.py

--- tests/test_STATE_valueConstraintType_alone.py
+++ tests/test_STATE_valueConstraintType_alone.py
@@ -4,10 +4,13 @@ import pytest
 from dctap.tapclasses import TAPStatementTemplate
 
 
-def test_valueConstraintType_warn_if_used_without_valueConstraint():
-    """@@@"""
+def test_valueconstrainttype_warn_if_used_without_valueconstraint():
+    """If valueConstraintType has value, but valueConstraint not, record a warning."""
     sc = TAPStatementTemplate()
     sc.propertyID = ":status"
     sc.valueConstraintType = "pattern"
+    sc.valueConstraint = ""
     sc._valueConstraintType_warn_if_used_without_valueConstraint()
+    warning = "Value constraint type 'pattern' has no corresponding value constraint."
     assert len(sc.state_warns) == 1
+    assert sc.state_warns.get("valueConstraint") == warning

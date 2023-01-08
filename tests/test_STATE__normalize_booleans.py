2f1fd79cef5a90511822b773cbef0ecbbbd33382
Tom Baker <tom@tombaker.org>
2023-01-08 11:05:44 +0100

    test_STATE__normalize_booleans.py

--- tests/test_STATE__normalize_booleans.py
+++ tests/test_STATE__normalize_booleans.py
@@ -6,7 +6,6 @@ from dctap.config import get_config
 from dctap.tapclasses import TAPStatementTemplate
 from dctap.inspect import pprint_tapshapes
 
-
 def test_mandatory_repeatable_true_given_supported_boolean_values():
     """Literal 'True' (case-insensitive) is a supported Boolean value."""
     sc = TAPStatementTemplate()
@@ -15,7 +14,6 @@ def test_mandatory_repeatable_true_given_supported_boolean_values():
     sc._normalize_booleans()
     assert sc.mandatory == "true"
     assert sc.repeatable == "false"
-
 
 def test_mandatory_and_repeatable_one_zero_normalized_to_true_false():
     """The integers 0 and 1 are supported Boolean values."""
@@ -26,7 +24,6 @@ def test_mandatory_and_repeatable_one_zero_normalized_to_true_false():
     assert sc.mandatory == "true"
     assert sc.repeatable == "false"
 
-
 def test_mandatory_and_repeatable_default_to_empty_strings():
     """The Boolean elements remain empty strings if no value is declared."""
     sc = TAPStatementTemplate()
@@ -34,13 +31,15 @@ def test_mandatory_and_repeatable_default_to_empty_strings():
     assert sc.mandatory == ""
     assert sc.repeatable == ""
 
-
 def test_mandatory_and_repeatable_raise_warn_unsupported_boolean_values():
     """Warn about unsupported Boolean values."""
     sc = TAPStatementTemplate()
     sc.mandatory = "WAHR"
-    sc.repeatable = "WAHR"
+    sc.repeatable = "FALSCH"
     sc._normalize_booleans()
     assert len(sc.state_warns) == 2
     assert sc.mandatory == "WAHR"
-    assert sc.repeatable == "WAHR"
+    assert sc.repeatable == "FALSCH"
+    assert sc.state_warns 
+    warning_for_true = "'WAHR' is not a supported Boolean value."
+    warning_for_false = "'FALSCH' is not a supported Boolean value."

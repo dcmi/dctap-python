a85b015d6b4e79407615087a17fb85d50f2eb8f0
Tom Baker <tom@tombaker.org>
2023-01-09 09:34:09 +0100

    test_STATE_elements_with_literals.py

---
+++ tests/test_STATE_elements_with_literals.py
@@ -0,0 +1,20 @@
+"""
+Tests TAPStatementTemplate._valueDataType_warn_if_used_with_valueNodeType_IRI
+
+Values must be literals (strings) if they have a value datatype.
+- If found not to be literals, records warning in sc.state_warns.
+"""
+
+import pytest
+from dctap.tapclasses import TAPStatementTemplate, TAPShape
+
+def test_warn_if_propertyID_not_URI():
+    """Values of propertyID should be IRI-like."""
+    sc = TAPStatementTemplate()
+    sc.valueNodeType = node_type = "iri"
+    sc.valueDataType = data_type = "xsd:integer"
+    sc._valueDataType_warn_if_used_with_valueNodeType_IRI()
+    assert len(dict(sc.state_warns)) == 1
+    warning = f"Datatype '{data_type}' incompatiable with node type '{node_type}'."
+    assert sc.state_warns["valueDataType"] == warning
+

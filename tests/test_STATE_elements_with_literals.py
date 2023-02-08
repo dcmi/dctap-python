15c0d867c079d4a80cf186c1b54811c731c8fe6a
Tom Baker <tom@tombaker.org>
2023-02-08 16:24:27 +0100

    tests for warnings when literal value used with value shape

--- tests/test_STATE_elements_with_literals.py
+++ tests/test_STATE_elements_with_literals.py
@@ -1,15 +1,17 @@
 """
-Tests TAPStatementTemplate._valueDataType_warn_if_used_with_valueNodeType_IRI
+Tests 
+- TAPStatementTemplate._valueDataType_warn_if_used_with_valueNodeType_IRI
+  Values must be literals (strings) if they have a value datatype.
+  - If found not to be literals, records warning in sc.state_warns.
 
-Values must be literals (strings) if they have a value datatype.
-- If found not to be literals, records warning in sc.state_warns.
+- TAPStatementTemplate._valueDataType_warn_if_used_with_valueShape
 """
 
 import pytest
 from dctap.tapclasses import TAPStatementTemplate, TAPShape
 
-def test_warn_if_propertyID_not_URI():
-    """Values of propertyID should be IRI-like."""
+def test_warn_if_nodetype_iri_used_with_any_valuedatatype():
+    """Node type IRI is incompatible with any (literal) value datatype."""
     sc = TAPStatementTemplate()
     sc.valueNodeType = node_type = "iri"
     sc.valueDataType = data_type = "xsd:integer"
@@ -18,3 +20,26 @@ def test_warn_if_propertyID_not_URI():
     warning = f"Datatype '{data_type}' incompatible with node type '{node_type}'."
     assert sc.state_warns["valueDataType"] == warning
 
+def test_warn_if_valueshape_used_with_any_valuedatatype(capsys):
+    """Use of a value shape is incompatible with any (literal) value datatype."""
+    sc = TAPStatementTemplate()
+    sc.valueDataType = data_type = "xsd:integer"
+    sc.valueShape = node_type = "my:UserShape"
+    sc._valueDataType_warn_if_used_with_valueShape()
+    assert len(dict(sc.state_warns)) == 1
+    warning = "Values with datatypes (literals) cannot conform to value shapes."
+    assert sc.state_warns["valueDataType"] == warning
+    # with capsys.disabled():
+    #     print()
+    #     print(sc.state_warns)
+
+def test_warn_if_valuedatatype_used_with_any_value_shape():
+    """Value with datatypes are literal, so cannot conform to a value shape."""
+    st = TAPStatementTemplate()
+    st.propertyID = ":status"
+    st.valueDataType = "xsd:date"
+    st.valueShape = "Person"
+    st._valueDataType_warn_if_used_with_valueShape()
+    assert len(st.state_warns) == 1
+    warning = "Values with datatypes (literals) cannot conform to value shapes."
+    assert st.state_warns["valueDataType"] == warning

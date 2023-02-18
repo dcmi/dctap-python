c9d476a1b9f4a5505bd178750c84c696b11ebcf9
Tom Baker <tom@tombaker.org>
2023-02-18 10:07:40 +0100

    csvreader.py - some simplification

--- dctap/csvreader.py
+++ dctap/csvreader.py
@@ -4,19 +4,11 @@ import re
 from collections import defaultdict
 from csv import DictReader
 from io import StringIO as StringBuffer
-from dataclasses import asdict
 from dctap.exceptions import DctapError, NoDataError
-from dctap.tapclasses import TAPShape, TAPStatementTemplate
 from dctap.utils import coerce_concise
 
 
-def csvreader(
-    csvfile_str=None,
-    config_dict=None,
-    open_csvfile_obj=None,
-    shape_class=TAPShape,
-    state_class=TAPStatementTemplate,
-):
+def csvreader(csvfile_str=None, config_dict=None, open_csvfile_obj=None):
     """Get list of CSV row dicts and related warnings."""
     if open_csvfile_obj:
         csvfile_contents_str = open_csvfile_obj.read()
@@ -26,16 +18,14 @@ def csvreader(
         raise NoDataError("No data to process.")
 
     tmp_buffer = StringBuffer(csvfile_contents_str)
-    csvlines_stripped = [
-        line.strip() for line in tmp_buffer if not re.match("#", line.strip())
-    ]
-    if len(csvlines_stripped) < 2:
+    csvlines = [line.strip() for line in tmp_buffer if not re.match("#", line.strip())]
+    if len(csvlines) < 2:
         raise NoDataError("No data to process.")
-    header_line_list = csvlines_stripped[0].split(",")
+    header_line_list = csvlines[0].split(",")
     header_line_list = _normalize_header_line(header_line_list, config_dict)
 
-    csv_warns = defaultdict(dict)
     recognized_elements = _list_recognized_elements(config_dict)
+    csv_warns = defaultdict(dict)
     for header in header_line_list:
         if header.lower() not in recognized_elements:
             warn = f"Non-DCTAP element '{header}' not configured as extra element."
@@ -43,14 +33,13 @@ def csvreader(
             csv_warns["csv"]["header"] = []
             csv_warns["csv"]["header"].append(warn)
 
-    new_header_line_str = ",".join(header_line_list)
-    csvlines_stripped[0] = new_header_line_str
-    if not csvlines_stripped[0]:
+    csvlines[0] = ",".join(header_line_list)
+    if not csvlines[0]:
         raise NoDataError("No data to process.")
-    if "propertyID" not in csvlines_stripped[0]:
+    if "propertyID" not in csvlines[0]:
         raise DctapError("Valid DCTAP CSV must have a 'propertyID' column.")
 
-    tmp_buffer2 = StringBuffer("".join([line + "\n" for line in csvlines_stripped]))
+    tmp_buffer2 = StringBuffer("".join([line + "\n" for line in csvlines]))
     csv_rows = list(DictReader(tmp_buffer2))
     for row in csv_rows:
         for key, value in row.items():
@@ -79,6 +68,16 @@ def _list_recognized_elements(config_dict):
     return [elem.lower() for elem in recognized_elements]
 
 
+def _normalize_element_name(some_str, element_aliases_dict=None):
+    """Given header string, return converted if aliased, else return unchanged."""
+    some_str = coerce_concise(some_str)
+    if element_aliases_dict:
+        for key in element_aliases_dict.keys():
+            if key == some_str:
+                some_str = element_aliases_dict[key]
+    return some_str
+
+
 def _normalize_header_line(header_line_list, config_dict):
     """Given raw header line list, return normalized."""
     new_header_line_list = []
@@ -89,13 +88,3 @@ def _normalize_header_line(header_line_list, config_dict):
         new_header_line_list.append(header)
 
     return new_header_line_list
-
-
-def _normalize_element_name(some_str, element_aliases_dict=None):
-    """Given header string, return converted if aliased, else return unchanged."""
-    some_str = coerce_concise(some_str)
-    if element_aliases_dict:
-        for key in element_aliases_dict.keys():
-            if key == some_str:
-                some_str = element_aliases_dict[key]
-    return some_str
